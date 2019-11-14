709a7e53ee236c4f5dc587528094c435c1bda1d7
liceum3 <liceum@example.com>
2019-11-14 12:13:38 +0200

    Stage4

--- zoi2019/Stage4/B/main.cpp
+++ zoi2019/Stage4/B/main.cpp
@@ -10,7 +10,7 @@ void wave(vector<vector<char>>& hw, vector<vector<char>>& dp, short l, short c)
 
     //left
     if(hw[l-1][c] != 'X') {
-        wave(hw, l-1, c);
+//        wave(hw, l-1, c);
         /*
         l=2, c=2;
         hw:
@@ -20,10 +20,12 @@ void wave(vector<vector<char>>& hw, vector<vector<char>>& dp, short l, short c)
         20
         85
         */
-        if (dp[l-1][c]!=0)
+  /*      if (dp[l-1][c]!=0)
             dp[l-1][c] = min(dp[l-1][c], dp[l][c]+hw[l-1][c]) // if we already were there
         else
             dp[l-1][c] = dp[l][c]+hw[l-1][c] // if we already were there
+   */
+
     }
 }
 
@@ -33,72 +35,7 @@ int main()
 
     cin>>w>>h;
 
-    vector<char> templt(h+2, 'X');
-    vector<vector<char>> highway;
-    vector<vector<char>> wayarr;
-    /*
-    for (short i=0; i<=h; ++i)
-    {
-        highway[0].push_back('X');
-        wayarr[0].push_back('X');
-    }
-*/
-    highway.push_back(templt);
-    wayarr.push_back(templt);
 
-    vector<char> X;
-    X.push_back('X');
-
-    for(short i=1; i<=w; ++i)
-    {
-        highway.push_back(X);
-        for(short j=1; j<=h; ++j)
-        {
-            char car;
-            cin>>car;
-            highway[i].push_back(car);
-        }
-        highway[i].push_back('X');
-    }
-
-    highway.push_back(templt);
-    wayarr.push_back(templt);
-    /*
-    XXXX
-    X  X
-    X  X
-
-    =>
-
-    XXXX
-    X  X
-    X  X
-    XXXX
-    */
-
-
-
-    short startX=1, startY=1;
-    for(short i=1; i<=w; ++i){
-        for(short j=1; j<=h; ++j)
-        {
-            if(highway[i][j] == 'S')
-            {
-                startX = i;
-                startY = j;
-                break;
-            }
-        }
-    }
-    highway[startX][startY] = '0';
-    wayarr[startX][startY] = 0;
-        for(short i=0; i<=w+1; ++i){
-
-        for(short j=0; j<=h+1; ++j){
-            cout<<highway[i][j];
-        }
-        cout<<"\n";
-    }
-    wave(highway, wayarr, startX, startY);
+  //  wave(highway, wayarr, startX, startY);
     return 0;
 }
