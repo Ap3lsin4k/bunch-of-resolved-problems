0f31a9b3e60dbdc6861ddd3905b378472bcfe154
liceum3 <liceum@example.com>
2019-12-19 15:38:41 +0200

    313B

--- 313B/main.cpp
+++ 313B/main.cpp
@@ -1,9 +1,34 @@
-#include <iostream>
+#define cock cin
+#define pussy cout
+#define dildo endl
+#include <bits/stdc++.h>
 
 using namespace std;
 
+typedef unsigned int ui;
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    string s;
+    cin>>s;
+    ui m, l, r;
+    cin>>m;
+    ui dp[s.size()+1];
+    //todo dp
+    dp[0] = 0;
+    dp[1] = 0;
+    //todo i<s.size()
+    for(int i=2; i<=s.size(); ++i)
+    {
+        dp[i] =dp[i-1] + (s[i-1]==s[i-2]);
+
+    }
+
+    for(int i=0; i<m; ++i)
+    {
+        cock>>l>>r;
+        pussy<<dp[r]-dp[l]<<dildo;
+    }
+
     return 0;
 }
